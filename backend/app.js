bf6a8dd831a074bc754cf98d39add7c3c36751bf
Ksr123456789 <kuldeeprawat6277@gmail.com>
2026-03-15 13:30:07 +0530

    register captain done

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const connectToDb = require("./db/db");
 const userRoutes = require("./routes/user.routes");
-
+const captainRoutes = require("./routes/captain.routes");
 connectToDb();
 
 app.use(cors());
@@ -19,5 +19,6 @@ app.get("/", (req, res) => {
 });
 
 app.use("/users", userRoutes);
+app.use("/captains", captainRoutes);
 
 module.exports = app;
